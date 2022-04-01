23ffc6acff3f3070ac047df9345287b8df977b14
leoafarias <leoafarias@users.noreply.github.com>
2022-04-01 15:25:00 +0000

    deploy: 38277b07e009789b79e271c4232033deb51bc6e2

---
+++ _next/static/-7jBRu43QcUeiVXutXjQv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/408-05c5e26e5041d6ce.js","static/chunks/pages/index-b4895a79d042a23d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
